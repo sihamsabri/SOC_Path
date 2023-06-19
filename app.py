3ec1854fb85a8b634888eacc3e57509cc66e1f3d
sihamsabri <67846060+sihamsabri@users.noreply.github.com>
2023-06-18 20:54:39 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -8,23 +8,34 @@ app = Flask(__name__, static_folder='static')
 def home():
     if request.method == 'POST':
         # Get the uploaded file
-        file = request.files['file']
-        file_path = file.filename
-        file.save(file_path)
-        # Here we get the extension of the file
-        file_extension = os.path.splitext(file_path)[1].lower()
-        if file_extension == '.pdf':
+        
+        if 'text' in request.form and 'file' in request.files:
+            text=request.form['text']
+            file = request.files['file']
+            txt= "python3 CAT/URL\ Detection/First_step/main.py "+text
+            txt_out= subprocess.check_output(txt, shell=True).decode("utf-8")
+            print(txt_out)
+            result = "Text Analysis result : "+ txt_out+ "\n"
+            file_path = file.filename
+            file.save(file_path)
+            # Here we get the extension of the file
+            file_extension = os.path.splitext(file_path)[1].lower()
+            if file_extension == '.pdf':
             
         # Process the file using your Python processing code
         #result = process_file(file)
-            com = "python3 pdf_analysis.py "+file_path
-            result = subprocess.check_output(com, shell=True)
-        elif file_extension == '.docx':
-            com = "python3 word.py "+file_path
-            result = subprocess.check_output(com, shell=True)
+                com = "python3 pdf_analysis.py "+file_path
+                pdf = subprocess.check_output(com, shell=True).decode("utf-8")
+                print(pdf)
+                result = result + "PDF File Analysis result: "+pdf+ "\n"
+            elif file_extension == '.docx':
+                com = "python3 word.py "+file_path
+                word = subprocess.check_output(com, shell=True)
+                print(word)
+                result= result + "Word File Analysis result: "+word+"\n"
         # Return the result to the HTML template
-        result = result.decode('utf-8')
-        return render_template('result.html', result=result)
+            #result = result.decode('utf-8')
+            return render_template('result.html', result=result)
 
     return render_template('upload.html')
 
